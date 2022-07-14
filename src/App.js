9698d8809920060a5459c70140938aa162da329f
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2022-07-14 17:03:45 -0500

    Implementacion de componente ItemListContainer en App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 // App principal
 import './App.css';
+// Importacion ItemListContainer
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
 // Componente del NavBar
 import NavBar from './components/NavBar/NavBar.js';
 
 function App() {
   return (
-   <NavBar/>
+    <><NavBar />
+      <ItemListContainer greeting="Estamos en construccion!" /></>
+
   );
 }
 
