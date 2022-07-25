b86caf58e6524833e7363133469d931a9f9574d3
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2022-07-25 15:32:08 -0500

    Working image routes, and working Item rendering

--- src/components/Items/Item/Item.js
+++ src/components/Items/Item/Item.js
@@ -3,20 +3,18 @@ import Card from 'react-bootstrap/Card';
 
 const Item = props => {
     return (
-        <>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={props.itemImg} />
-                <Card.Body>
-                    <Card.Title>{props.itemName}</Card.Title>
-                    <Card.Text>
-                        {props.itemDescription}
-                    </Card.Text>
-                    <Card.Text>
-                        {props.itemStock}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src= {props.itemImg} />
+            <Card.Body>
+                <Card.Title>{props.itemName}</Card.Title>
+                <Card.Text>
+                    {props.itemDescription}
+                </Card.Text>
+                <Card.Text>
+                    {props.itemStock}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
--- src/components/Items/ItemListContainer/ItemListContainer.js
+++ src/components/Items/ItemListContainer/ItemListContainer.js
@@ -5,19 +5,27 @@ import "./ItemListContainer.css";
 import Container from 'react-bootstrap/Container';
 import ItemList from "../ItemList/ItemList";
 
+import React from "react";
+import { useEffect, useState } from "react";
+
+
 
 
 const ItemListContainer = (props) => {
 
-    const items = [];
+    const productsURL = "https://mafty-shop-default-rtdb.firebaseio.com/productos.json";
 
-    const fetchItems = () => {
-        return [];
-    }
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch(productsURL).then(response => response.ok ? response.json() : Promise.reject("Error al cargar datos."))
+            .then(data => setItems(data))
+            .catch(err => console.log(err));
+    }, []);
 
     return <Container>
         <h1 className="msj">{props.greeting}</h1>
-        <ItemList items={items} />
+        <ItemList items={items}/>
     </Container>;
 }
 
