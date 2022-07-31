77b496432605550553cb3e4639a89151d0de0e50
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2022-07-30 19:10:53 -0500

    Modified classes for responsiveness and added back button

--- src/components/Items/ItemDetail/ItemDetail.js
+++ src/components/Items/ItemDetail/ItemDetail.js
@@ -10,14 +10,19 @@ import "./ItemDetail.css"
 const ItemDetail = ({ itemImg, itemName, itemStock, itemLongDescription, itemPrice }) => {
 
     return <Row className="itemDetail">
+        <div className="backButtonContainer">
+            <button className="backButton"><i className="fa-solid fa-angle-left"></i></button>
+        </div>
         <Col xs={12} md={9} lg={5}>
             <img className="itemImg" src={itemImg} alt="Product" />
         </Col>
         <Col lg={7}>
             <Row className="itemRow">
-                <h1 className="itemName">{itemName}</h1>
-                <h2 className="itemPrice">{`$${itemPrice} (MXN)`}</h2>
-                <p className="itemDescription">{itemLongDescription}</p>
+                <div className="itemDetails">
+                    <h1 className="itemName">{itemName}</h1>
+                    <h2 className="itemPrice">{`$${itemPrice} (MXN)`}</h2>
+                    <p className="itemDescription">{itemLongDescription}</p>
+                </div>
                 <div className="itemCount">
                     <ItemCount initial={1} stock={itemStock} />
                 </div>
