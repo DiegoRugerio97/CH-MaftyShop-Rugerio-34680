2a17c160a0fc3f07ca3f69fc7b7fcd1a8834ed42
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2022-08-14 14:36:42 -0500

    Addint firestore and firebase connection to app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Firebase
+import { initializeApp } from "firebase/app";
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYoqU1ZMI5883OmcTbe6a3PyVCh49_tXs",
+  authDomain: "mafty-shop.firebaseapp.com",
+  databaseURL: "https://mafty-shop-default-rtdb.firebaseio.com",
+  projectId: "mafty-shop",
+  storageBucket: "mafty-shop.appspot.com",
+  messagingSenderId: "202157372657",
+  appId: "1:202157372657:web:62dee23937bfbde6c7a550"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
