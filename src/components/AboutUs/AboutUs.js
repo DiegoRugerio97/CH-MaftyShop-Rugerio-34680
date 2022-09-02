59f4bb508eb24b1cee2d029c3e03efa23db6ea4d
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2022-09-02 09:34:31 -0500

    Detailed aboutUs page

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,10 +1,23 @@
+// AboutUs component - Present information about component
+// BS imports
+import Container from 'react-bootstrap/Container';
+// Styling
 import "./AboutUs.css";
+// Image
+import Photo from "../../img/img2.jpg";
 
 const AboutUs = () => {
-    return <div className="constructionPlaceholder">
-    <h1>Estamos en construccion!</h1>
-    <img src="/img/construccion.jpg" alt="Construccion"/>
-</div>
+    return <Container className='about'>
+        <h1 className='about-title'>Somos Mafty Shop</h1>
+        <article className='about-content'>
+            Somos una tienda dedicada a traerte lo mejor del mundo del anime al mejor precio.
+            <br/>
+            Nos especializamos en figuras de tus personajes favoritos, modelos a escala del anime Gundam (Gunpla) y las series de manga mas populares.
+            <br/>
+            <strong>¡Todo directamente importado desde Japón!</strong>
+            <img className='about-image' src={Photo} alt="Anime Shop"/>
+        </article>
+    </Container>
 }
 
 export default AboutUs;
